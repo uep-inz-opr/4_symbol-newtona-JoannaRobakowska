171e21cb23167f8de379d02647df8e7e151cef49
Joanna <86994@student.ue.poznan.pl>
2022-04-28 22:35:15 +0200

    Pierwsze podejście

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ t = int( raw_input() )
 
 for i in range(t):
 	n,k = map( int, raw_input().split() )
-	if k == 0 or k == n : print '1'
+	if k == 0 or k == n : 
+		print ("1")
 	else : print Newton(n,k)
