7b8f2d12c123c36f85181b7611228b97a58b2565
Joanna <86994@student.ue.poznan.pl>
2022-04-28 22:33:23 +0200

    Pierwsze podejście

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+print ("Aby obliczyć silnię, podaj dwie liczby oddzielone spacją:")
+def Newton( n, k ):
+
+	Wynik = 1
+	for i in range( 1, k+1 ):
+		Wynik = Wynik * ( n - i + 1 ) / i
+	return Wynik
+
+t = int( raw_input() )
+
+for i in range(t):
+	n,k = map( int, raw_input().split() )
+	if k == 0 or k == n : print '1'
+	else : print Newton(n,k)
