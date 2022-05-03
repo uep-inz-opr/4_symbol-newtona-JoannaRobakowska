9d455dd526f298b2c18f11c3a40608e8dddd1d5d
Joanna <86994@student.ue.poznan.pl>
2022-05-03 14:41:08 +0200

    Pierwsze podejście

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
 print ("Aby obliczyć silnię, podaj dwie liczby oddzielone spacją:")
-    def dwumianREK(n, k):
         if k == 0 or k == n:
-            return 1
+            print("1")
         else:
-            return float(n)*dwumianRek(n-1,k-1)/k
+            print (math.comb(n, k))
