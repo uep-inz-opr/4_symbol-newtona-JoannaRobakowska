6fb758d6e809e3b656fd7f2899707a2ce108198b
Joanna <86994@student.ue.poznan.pl>
2022-05-03 20:46:58 +0200

    Pierwsze podejście

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import math
 liczby = input("Aby obliczyć silnię, podaj dwie liczby oddzielone spacją:")
 liczby = liczby.split(" ")
-def newton(n,k):
-	return (math.comb(n, k))
+def dwum(n,k):
+    wynik=1
+    for i in range (1,k):
+        wynik=wynik*(n-i+1)/i
+    return wynik
+ 
+while(t): 
+    n,k=input().split()    
+    n=int(n)
+    k=int(k)
+    print(dwum(n,k))
