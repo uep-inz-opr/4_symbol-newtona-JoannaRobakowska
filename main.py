bb924a17f6d777053b8e97953fc2fd191368d276
Joanna <86994@student.ue.poznan.pl>
2022-05-03 14:43:04 +0200

    Pierwsze podejście

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 import math
 print ("Aby obliczyć silnię, podaj dwie liczby oddzielone spacją:")
-        if k == 0 or k == n:
-            print("1")
-        else:
-            print (math.comb(n, k))
+print (math.comb(n, k))
