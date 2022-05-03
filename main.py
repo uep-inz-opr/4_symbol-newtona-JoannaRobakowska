b5b17ccc7fe98f1641646c599b75f6d4d16eef87
Joanna <86994@student.ue.poznan.pl>
2022-05-03 20:43:20 +0200

    Pierwsze podejście

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
 liczby = input("Aby obliczyć silnię, podaj dwie liczby oddzielone spacją:")
 liczby = liczby.split(" ")
-	def newton(n,k):
-		return (math.comb(n, k))
+def newton(n,k):
+	return (math.comb(n, k))
