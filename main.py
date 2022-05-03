b0a7b7bce41242d0287209b19f5ca62d96a69685
Joanna <86994@student.ue.poznan.pl>
2022-05-03 19:50:52 +0200

    Pierwsze podejście

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import math
 wprowadzenie= input("Aby obliczyć silnię, podaj dwie liczby oddzielone spacją:")
-	wprowadzenie = int(n), iny(k)
+	wprowadzenie = int(n), int(k)
 print (math.comb(n, k))
