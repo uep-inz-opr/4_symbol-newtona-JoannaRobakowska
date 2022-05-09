2ac114c05dfb69de19b9b77c680cfa355fc2cc31
Joanna <86994@student.ue.poznan.pl>
2022-05-09 20:59:20 +0200

    Pierwsze podejście

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 import math
-liczby = input("Aby obliczyć silnię, podaj dwie liczby oddzielone spacją:")
-liczby = liczby.split(" ")
-def dwum(n,k):
-    wynik=1
-    for i in range (1,k):
-        wynik=wynik*(n-i+1)/i
-    return wynik
+def silnia(n, k):
+	return factorial(n) // (factorial(k) * factorial(n-k))
+	print (silnia(6, 4))
+
+#t=int(stdin.readline());
+#t = int(input())
  
-while(t): 
-    n,k=input().split()    
-    n=int(n)
-    k=int(k)
-    print(dwum(n,k))
+#for i in range(t):
+#	n,k = map(int, input().split())
+#	if k == 0 or k == n : print ("1")
+#	else : print (silnia(n,k))
+
+#while(t):
+	#n,k=input().split()
+	#n=int(6)
+	#k=int(4)
+	#print(silnia(n,k))
+#liczby = input("Aby obliczyć silnię, podaj dwie liczby oddzielone spacją:")
+l#iczby = liczby.split(" ")
+#def dwum(n,k):
+ ##  for i in range (1,k):
+   #     wynik=wynik*(n-i+1)/i
+    #return wynik
+ 
+#while(t): 
+ #   n,k=input().split()    
+  #  n=int(n)
+   # k=int(k)
+    #print(dwum(n,k))
+
+
+#    import math
+#print ("podaj wartości oddzielone spacją")
+#	if a<0 or b<0 or c<0
+#	print ("błędne dane")
+###		print (wynik)
+#	if a=int, b=int, c=0:
+#		wynik=a*b
+#		print (wynik)
+#	if a=int, b=int, c=int:
+#		p=(a+b+c)/2
+##		print (wynik)
+
+
